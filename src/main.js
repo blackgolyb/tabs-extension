c6658fe65a1d149368ada542a2319f8ec945394e
Blackgolyb <andrejomelnickij@gmail.com>
2024-12-14 01:47:41 +0200

    create functions for open and close popup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log("test");
+let isTabsPopupOpen = false;
+
+function createPopup() {
+    isTabsPopupOpen = true;
+    console.log("open popup");
+}
+
+function removePopup() {
+    isTabsPopupOpen = false;
+    console.log("remove popup");
+}
+
+document.addEventListener("keydown", (event) => {
+    if (!isTabsPopupOpen && event.ctrlKey && event.key === " ") {
+        createPopup();
+    } else if (isTabsPopupOpen && event.key === "Escape") {
+        removePopup();
+    }
+});
