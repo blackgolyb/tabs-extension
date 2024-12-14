0a73975e27b6d0848db28feaccce2d254b1cc651
Blackgolyb <andrejomelnickij@gmail.com>
2024-12-14 02:28:43 +0200

    create popup window

--- src/main.js
+++ src/main.js
@@ -1,13 +1,49 @@
+/**
+ * @param {String} HTML representing a single node (which might be an Element,
+                   a text node, or a comment).
+ * @return {Node}
+ */
+function htmlToNode(html) {
+    const template = document.createElement("template");
+    template.innerHTML = html.trim();
+    const nNodes = template.content.childNodes.length;
+    if (nNodes !== 1) {
+        throw new Error(
+            `html parameter must represent a single node; got ${nNodes}. ` +
+                "Note that leading or trailing spaces around an element in your " +
+                'HTML, like " <img/> ", get parsed as text nodes neighbouring ' +
+                "the element; call .trim() on your input to avoid this.",
+        );
+    }
+    return template.content.firstChild;
+}
+
+const extensionHTML = `
+<div class="tabs-extension">
+    <div class="content">
+        <div class="search">
+            <input type="text" placeholder="Search..." />
+        </div>
+        <div class="tabs-list"></div>
+    </div>
+</div>
+`;
+
 let isTabsPopupOpen = false;
+let popupRef;
 
 function createPopup() {
     isTabsPopupOpen = true;
-    console.log("open popup");
+    popupRef = htmlToNode(extensionHTML);
+    document.body.appendChild(popupRef);
 }
 
 function removePopup() {
     isTabsPopupOpen = false;
-    console.log("remove popup");
+    if (popupRef) {
+        document.body.removeChild(popupRef);
+        popupRef = undefined;
+    }
 }
 
 document.addEventListener("keydown", (event) => {
